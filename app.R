42cad1e6a7d1f8b5c32c750d3de95f0881a83087
pyroman1337 <romanhppi@gmail.com>
2018-08-31 14:00:36 +0200

    reactivity depends on action button all inputs in ppb time separated in semicolons

--- app.R
+++ app.R
@@ -37,11 +37,11 @@ ui <- fluidPage(
          h4("Please provide the input about your GHG chamber system"),
          
          numericInput(inputId="GC.sd", label=HTML("GC.sd [ppb]"), value = 5, width = 100),         
-         helpText("Standard deviation of at least 50 close to atmospheric standard samples"),
-         textInput(inputId="timing",label="sample timing [h]", value = "0, 0.25, 0.5, 0.75"),
-         helpText("Enter the time of your samples comma separated",
+         helpText("Standard deviation of at least 50 standard samples (close to atmospheric concentration)"),
+         textInput(inputId="timing",label="sample timing [h]", value = "0; 0.25; 0.5; 0.75"),
+         helpText("Enter the time of your samples separated by semicolon",
                  "in the unit of your choice but prefereably hours",
-                 "number of sample from 2 to n"),
+                 "(number of samples supported from 2 to n)"),
          numericInput3(inputId="area", label=HTML("chamber area &emsp; [m<sup>2</sup>]"), value = 0.0491, width = 100),
          numericInput3(inputId="volume", label=HTML("chamber volume [m<sup>3</sup>]"), value = 0.00245, width = 100),
          
@@ -49,8 +49,9 @@ ui <- fluidPage(
                      choices = c("Nitrous oxide" = "N2O",    # eval(c(expression("N"[2]*"O"))
                                  "Carbon dioxide" = "CO2",
                                  "Methane" = "CH4"),
-                     selected = 'N2O')
-         
+                     selected = 'N2O'),
+        actionButton("gasfluxes.go", "Simulate fluxes")
+        
       ),
       
       # Show a plot of the generated distribution
@@ -74,17 +75,20 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
+    
    flxlim.sim <- reactive({
-  
+     
      # GC.sd = 3
      # t = seq(0, 0.5, length.out =  4)
      # V = 0.00245
      # A = 0.0491
+     # observeEvent(input$gasfluxes.go, {
+     input$gasfluxes.go
+     
      C0.ppb <- switch(input$gas.species,
                         "N2O" = 330,  # ppb
-                        "CO2" = 400 , # ppm
-                        "CH4" = 1800)# ppb
+                        "CO2" = 400000 , # ppb
+                        "CH4" = 1800) # ppb
      
      species.mol <- switch(input$gas.species,
                       "N2O" = 44,  # g/mol
@@ -93,10 +97,8 @@ server <- function(input, output) {
  
      C0   <- C0.ppb      * species.mol * 273.15 / 22.4 / (273.15 + 15) #mg / m^3 at 15°C for N2O/CH4
      sdGC <- input$GC.sd * species.mol * 273.15 / 22.4 / (273.15 + 15) #mg / m^3 
-     # set.seed(42)
-     t <- as.numeric(unlist(strsplit(input$timing,",")))
-     
-
+     set.seed(55)
+     t <- as.numeric(unlist(strsplit(input$timing,";")))
      
      ## hungary chambers: A = 0.0491, V =	0.00245
      
@@ -110,37 +112,58 @@ server <- function(input, output) {
      f.detect <<- simflux[, quantile(f0, 0.975)] #0
      # f.detect
      n.HMR <- simflux[!is.na(HMR.f0), .N] #
-     flxlim.out <- list(f.detect = f.detect, n.HMR = n.HMR, simflux = simflux)   
+     flxlim.out <- list(f.detect = f.detect, n.HMR = n.HMR, simflux = simflux)  
+     # }, once = F)  # 
+     
      return(flxlim.out)
-   })
-  
-   output$distPlot <- renderPlotly({
-      # generate bins based on input$bins from ui.R
-     simflux <- flxlim.sim()$simflux
-     f.detect <- flxlim.sim()$f.detect
-     flux.unit  <- switch(input$gas.species,
-                          "N2O" = "[mg N<sub>2</sub>O m<sup>-2</sub> h<sup>-1</sub> ]", 
-                          "CO2" = "[ g CO<sub>2</sub> m<sup>-2</sub> h<sup>-1</sub> ]", 
-                          "CH4" = "[mg CH<sub>4</sub> m<sup>-2</sub> h<sup>-1</sub> ]")
-    
-     ggplotly(
-       ggplot(simflux, aes_string(x = "id", y = "f0")) +
-       geom_hline(yintercept = 0, col = "green") +   ylim(-2*f.detect,2*f.detect) +
-       geom_point(size = 0.9) + geom_hline(yintercept = c(f.detect,-f.detect), col = "red") +
-       ylab(paste("flux",flux.unit)) #+ xlab("ΔN<sub>2</sub>O [ppb]")  #expression(
-       ,height = 450, dynamicTicks = T)  # )
      
    })
    
+
+   # # run.gasflx <- reactive({
+   #   withProgress(message = ' Calculating gas fluxes. Please wait ...', {
+   #     # session$sendCustomMessage(type = 'testmessage',
+   #     #                           message = 'eimalzwätschgezweimalzwätschgedrümalzwätschge... mach ma hüüüü')
+   #     simflux
+   #   # })
+   # })
+
+   
+   output$distPlot <- renderPlotly({
+      # generate bins based on input$bins from ui.R
+     input$gasfluxes.go
+     isolate({
+       simflux <- flxlim.sim()$simflux  
+       f.detect <- flxlim.sim()$f.detect
+       flux.unit  <- switch(input$gas.species,
+                            "N2O" = "[mg N<sub>2</sub>O m<sup>-2</sub> h<sup>-1</sub> ]", 
+                            "CO2" = "[mg CO<sub>2</sub> m<sup>-2</sub> h<sup>-1</sub> ]", 
+                            "CH4" = "[mg CH<sub>4</sub> m<sup>-2</sub> h<sup>-1</sub> ]")
+       
+       ggplotly(
+         ggplot(simflux, aes_string(x = "id", y = "f0")) +
+           geom_hline(yintercept = 0, col = "green") +   ylim(-2*f.detect,2*f.detect) +
+           geom_point(size = 0.9) + geom_hline(yintercept = c(f.detect,-f.detect), col = "red") +
+           ylab(paste("flux",flux.unit)) #+ xlab("ΔN<sub>2</sub>O [ppb]")  #expression(
+         ,height = 450, dynamicTicks = T)  # )
+       
+     })
+    
+   })
+   
    output$value <- renderPrint({
+     input$gasfluxes.go
      # flux.unit  <- switch(input$gas.species,
      #                      "N2O" = expression(paste("mg ",CO_2*m^2*h^2)), 
      #                      "CO2" = "[ g CO<sub>2</sub> m<sup>-2</sub> h<sup>-1</sub> ]", 
      #                      "CH4" = "[mg CH<sub>4</sub> m<sup>-2</sub> h<sup>-1</sub> ]")
      # 
-     invisible(print("Estimated minimal detectable flux:"));
-     print(flxlim.sim()$f.detect); # print(flux.unit);
-     print("Number of HMR fluxes detected:");flxlim.sim()$n.HMR})
+     isolate({
+       invisible(print("Estimated minimal detectable flux:"));
+       print(flxlim.sim()$f.detect); # print(flux.unit);
+       print("Number of HMR fluxes detected:");flxlim.sim()$n.HMR})
+     })
+   
 }
 
 # Run the application 
